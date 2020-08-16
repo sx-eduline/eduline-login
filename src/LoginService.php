1277e6c12d1edc7b10ed48bda3431f349b3aaa80
sunyonghong <syh@sunyonghong.com>
2020-08-16 17:07:34 +0800

    登陆服务类

---
+++ src/LoginService.php
@@ -0,0 +1,23 @@
+<?php
+namespace eduline\login;
+
+use think\facade\Route;
+use think\Service;
+
+class LoginService extends Service
+{
+    public function boot()
+    {
+        $this->registerRoutes(function () {
+            /** 接口路由 */
+            Route::group('system/package/login', function () {
+                // 登陆网关列表
+                Route::get('/list', 'index');
+                // 配置页面
+                Route::get('/<gateway>/config', 'config')->pattern(['gateway' => '[a-zA-Z_]+']);
+                // 启用配置
+                Route::post('/<gateway>/status', 'changeStatus')->pattern(['gateway' => '[a-zA-Z_]+']);
+            })->prefix('\eduline\login\admin\service\Config@')->middleware(['adminRoute']);
+        });
+    }
+}
